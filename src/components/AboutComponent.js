cf3011adf2f9b0463026d93df1652980cec2afd7
Gelin Nguedong <gelinhope@hotmail.com>
2021-04-22 09:43:54 +0200

    React Router and Single Page Applications

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,21 +2,18 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, CardImg, CardTitle, CardSubtitle, CardText, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderLeader({ image, name, designation, description }) {
+function RenderLeader({ leader }) {
     return (
-        <div className="row no-gutters">
-            <div className="col-12 col-sm-2">
-                <img width="60%" src={image} alt={name}/>
-            </div>
-            <div className="col-12 col-sm-10 offset-md-0">
-                <p>
-                    <b>{name}</b>
-                    {designation ? <p>{designation}</p> : null}
-                </p>
-                <p>{description}</p>
-            </div>
-        </div>
-
+        <Media tag="li" key={leader.id} className="col-12 mt-1" >
+            <Media left middle>
+                <Media object src={leader.image} alt={leader.name} />
+            </Media>
+            <Media body className="ml-5">
+                <Media heading>{leader.name}</Media>
+                <p>{leader.designation}</p>
+                <p>{leader.description}</p>
+            </Media>
+        </Media>
     );
 }
 
@@ -25,7 +22,7 @@ function About(props) {
     const leaders = props.leaders.map((leader) => {
         return (
             <div key={leader.id}>
-                <RenderLeader image={leader.image} name={leader.name} designation={leader.designation} description={leader.description} />
+                <RenderLeader leader={leader} />
             </div>
         );
     });
