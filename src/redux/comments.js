ff1491e9421e868a2f299387fb542cd269a481b8
Gelin Nguedong <gelinhope@hotmail.com>
2021-06-08 15:11:30 +0200

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from '../shared/comments.js';
+
+export const Comments = (state = COMMENTS, action) => {
+    switch (action.type) {
+
+        default:
+          return state;
+      }
+};
